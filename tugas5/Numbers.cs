3406efcc005d664adaac62dc46dcf3c80eccd78a
winda <windaardelia@students.amikom.ac.id>
2023-04-09 13:25:57 +0700

    program max min

---
+++ tugas5/Numbers.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tugas5
+{
+    internal class Numbers
+    {
+        public void CariNilaiMin(int a, int b, int c)
+        {
+            Console.WriteLine("Tersedia angka sebagai berikut {0}, {1}, {2}", a, b, c);
+
+            if (a <= b && a <= c)
+            {
+                
+                Console.WriteLine("Nilai terkecil dari ketiga angka adalah: {0}", a);
+            }
+            else if (b <= a && b <= c)
+            {
+               
+                Console.WriteLine("Nilai terkecil dari ketiga angka adalah: {0}", b);
+            }
+            else if (c <= a && c <= b)
+            {
+                
+                Console.WriteLine("Nilai terkecil dari ketiga angka adalah: {0}", c);
+
+            }
+        }
+
+        
+        public int CariNilaiMax(int a, int b, int c)
+        {
+            Console.WriteLine("Tersedia angka sebagai berikut {0}, {1}, {2}", a, b, c);
+           
+            if (a >= b && a >= c)
+            {
+                
+                Console.WriteLine("Nilai terbesar dari ketiga angka adalah : {0}", a);
+            }
+            else if (b >= a && b >= c)
+            {
+                
+                Console.WriteLine("Nilai terbesar dari ketiga angka adalah: {0}", b);
+            }
+            else if (c >= a && c <= b)
+            {
+               
+                Console.WriteLine("Nilai terbesar dari ketiga angka adalah: {0}", c);
+            }
+            return 0;
+        }
+    }
+}
---
+++ tugas5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using tugas5;
+
+namespace Pertemuan5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Numbers method = new Numbers();
+            method.CariNilaiMin(2, 8, 1);
+            
+            Console.WriteLine("--------------------------------------");
+            method.CariNilaiMax(4, 5, 3);
+           
+            Console.ReadKey();
+        }
+    }
+}
